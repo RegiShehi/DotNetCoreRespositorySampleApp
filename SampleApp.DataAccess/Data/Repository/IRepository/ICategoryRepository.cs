b99042d3c14aacb6fc1a290e714e6edffe9da9fa
Regi Shehi <shehi.regi@gmail.com>
2020-01-12 11:56:46 +0100

    Add Repository Pattern

---
+++ SampleApp.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SampleApp.DataAccess.Data.Repository.IRepository;
+using SampleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SampleApp.DataAccess.Data.Repository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        IEnumerable<SelectListItem> GetCategoryListForDropDown();
+        void Update(Category category);
+    }
+}
